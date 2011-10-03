cdc75f17e606962fd63b694219822666b8588a2c
Alex Li <alex.softb@gmail.com>
2011-10-03 18:42:33 +0300

    edit index files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Firstapp::Application.routes.draw do
-  get "index/index"
+  get "welcome/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +50,7 @@ Firstapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'index#index'
+  #root :to => 'index#index'
 
   # See how all your routes lay out with "rake routes"
 
