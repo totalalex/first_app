c148b61755056a3a41e291ea5e29310e7571ce18
Alex Li <alex.softb@gmail.com>
2011-10-03 15:54:52 +0300

    Initial commit

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,6 @@
+class IndexController < ApplicationController
+  def index
+    @users = User.find(:all)
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firstapp::Application.config.secret_token = '7cc0dec48a5973ba820a6529cdbc184a1935b3c04ad6911555ccc45016dc2698ae07f14eca31eee281099b8b1783e6b39d282a9cd62e2429e7b4cab5545780b8'
